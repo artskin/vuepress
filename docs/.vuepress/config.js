253f4fb2b8223896cb60a524a13d926c5aa15ff8
guliqiang <guliqiang@sensetime.com>
2019-11-20 13:09:53 +0800

    add notes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  title: "Vuepress",
-  description: "This is a Zeit Now 2.0 example",
+  title: "学习笔记",
+  description: "阿沐的学习资料整理，Document library",
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
       { text: "Guide", link: "/guide/" },
-      { text: "Config Page", link: "/config" }
+      { text: "Config Page", link: "/config" },
+	  { text: '学习文档', link: '/notes/' }
     ]
   },
-  dest: "public"
+  dest: "public",
+  sidebar: 'auto'
 };
