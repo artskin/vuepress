fb5720f7d9e7c59e10909621a12c67b555a7558b
Guliqiang <guliqiang@sensetime.com>
2019-12-02 19:27:30 +0800

    add index.ssr.html

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no' }]
   ],
   title: "前端笔记",
-  description: "阿沐的学习资料整理，Document library",
+  description: "阿沐的学习资料整理，Document library2",
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
